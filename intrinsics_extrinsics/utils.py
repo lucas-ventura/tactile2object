085d7fc2999ed6cad8123f4f00ae0c0f9a554d4c
lucasventurar <lucas.ventura.r@gmail.com>
2021-07-15 12:47:25 +0200

    Add viewpoint

--- intrinsics_extrinsics/utils.py
+++ intrinsics_extrinsics/utils.py
@@ -400,3 +400,26 @@ class AprilTags:
         single_apriltag = AprilTag(img_pth, intrinsic_params)
 
         return single_apriltag.get_image(radius=radius, thickness=thickness, show=show)
+
+
+def save_view_point(pcd, filename):
+    # https://github.com/intel-isl/Open3D/blob/2b8712ea8824f2010c4c5235d9ad8a7f7e40681d/examples/Python/Advanced/load_save_viewpoint.py
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    vis.add_geometry(pcd)
+    vis.run()  # user changes the view and press "q" to terminate
+    param = vis.get_view_control().convert_to_pinhole_camera_parameters()
+    o3d.io.write_pinhole_camera_parameters(filename, param)
+    vis.destroy_window()
+
+
+def load_view_point(pcd, filename):
+    # https://github.com/intel-isl/Open3D/blob/2b8712ea8824f2010c4c5235d9ad8a7f7e40681d/examples/Python/Advanced/load_save_viewpoint.py
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    ctr = vis.get_view_control()
+    param = o3d.io.read_pinhole_camera_parameters(filename)
+    vis.add_geometry(pcd)
+    ctr.convert_from_pinhole_camera_parameters(param)
+    vis.run()
+    vis.destroy_window()
