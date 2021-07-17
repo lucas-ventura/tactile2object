cc7c9533b4652888cf1fd2c3d46e2f3d1db47f8d
lucasventurar <lucas.ventura.r@gmail.com>
2021-07-17 20:24:09 +0200

    Add function to generate a rectangle from the corners

--- intrinsics_extrinsics/utils.py
+++ intrinsics_extrinsics/utils.py
@@ -520,6 +520,28 @@ def get_balls_from_corners(corners_w, color=[0, 1, 0]):
     return corners_balls
 
 
+def get_rectangle_from_corners(corners_w, color=[1, 0, 0]):
+    triangles = np.array(([4, 6, 5],
+                          [4, 7, 6],
+                          [0, 3, 4],
+                          [3, 7, 4],
+                          [0, 1, 3],
+                          [1, 2, 3],
+                          [1, 5, 6],
+                          [1, 6, 2],
+                          [3, 2, 6],
+                          [3, 6, 7],
+                          [0, 4, 1],
+                          [1, 4, 5]))
+
+    rectangle = o3d.geometry.TriangleMesh()
+    rectangle.vertices = o3d.utility.Vector3dVector(corners_w)
+    rectangle.triangles = o3d.utility.Vector3iVector(triangles)
+    rectangle.paint_uniform_color(color)
+
+    return rectangle
+
+
 def rigid_transform_3D(A, B):
     # Code from https://github.com/nghiaho12/rigid_transform_3D/blob/master/rigid_transform_3D.py
     assert A.shape == B.shape
